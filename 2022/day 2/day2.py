c61aa0c4d51947e45c19a2721be29614db9aaca3
Ruszrok <judanov.v@gmail.com>
2022-12-02 22:50:01 +0700

    last refactoring

--- 2022/day 2/day2.py
+++ 2022/day 2/day2.py
@@ -11,12 +11,16 @@ def parse_input(path : str) -> List[Tuple[int, int]]:
     with io.open(path, 'r') as file:
         for line in file.readlines():
             parts = [s.strip() for s in line.split(' ')]
-            result.append((DICT_FIRST[parts[0]], DICT_SECOND[parts[1]]))
+            result.append((parts[0],parts[1]))
 
     return result
 
-GAME_SCORE_1 = {(1,1):4, (1,2):8,(1,3):3,(2,1):1,(2,2):5,(2,3):9,(3,1):7,(3,2):2,(3,3):6}
-GAME_SCORE_2 = {(1,1):3, (1,2):4,(1,3):8,(2,1):1,(2,2):5,(2,3):9,(3,1):2,(3,2):6,(3,3):7}
+GAME_SCORE_1 = {('A','X'):4, ('A','Y'):8,('A','Z'):3,
+                    ('B','X'):1,('B','Y'):5,('B','Z'):9,
+                    ('C','X'):7,('C','Y'):2,('C','Z'):6}
+GAME_SCORE_2 = {('A','X'):3, ('A','Y'):4,('A','Z'):8,
+                    ('B','X'):1,('B','Y'):5,('B','Z'):9,
+                    ('C','X'):2,('C','Y'):6,('C','Z'):7}
 
 if __name__ == "__main__":
     input = parse_input("input.txt")
