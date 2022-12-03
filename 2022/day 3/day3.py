e226b9bfb52faf25c21bce9809d2eb6c980ad498
Ruszrok <judanov.v@gmail.com>
2022-12-03 19:20:34 +0700

    Second implemented

--- 2022/day 3/day3.py
+++ 2022/day 3/day3.py
@@ -18,8 +18,7 @@ def to_priority(code: str) -> int:
     else:
         return c - 64 + 26
 
-if __name__ == "__main__":
-    input = parse_input("input.txt")
+def first_task_solution(input: List[str]) -> int:
     result = 0
     for r in input:
         items = {}
@@ -30,6 +29,32 @@ if __name__ == "__main__":
             if(i in items):
                 result += to_priority(i)
                 break
-    #print(result, "Expected: 157")
-    print(result)
+    return result
+
+def second_task_solution(input: List[str]) -> int:
+    result = 0
+    for r1, r2, r3 in zip(*[iter(input)] * 3):
+        items = {}
+        for i in r1:
+            items[i] = 1
+        
+        for i in r2:
+            if(i in items):
+                items[i] = 2
+        
+        for i in r3:
+            if(i in items and items[i] == 2):
+                result += to_priority(i)
+                break
+
+
+    return result
+
+
+if __name__ == "__main__":
+    input = parse_input("input.txt")
+    result1 = first_task_solution(input)
+    result2 = second_task_solution(input)
+    print(result1)
+    print(result2)
         
