6da49aeb5c3a84fa394d957d79f9822ea7a0684f
Ruszrok <judanov.v@gmail.com>
2022-12-04 21:34:05 +0700

    Fixed mistakes in parsing

--- 2022/day 4/day4.py
+++ 2022/day 4/day4.py
@@ -6,19 +6,22 @@ from typing import List
 from typing import Tuple
 
 class Range:
-    def __init__(self, start, finish) -> None:
+    def __init__(self, start : int, finish : int) -> None:
         self.start = start
         self.finish = finish
     
     def contains(self, r: Range) -> bool:
         return self.start <= r.start and self.finish >= r.finish
+
+    def __str__(self) -> str:
+        return f'{self.start}-{self.finish}'
         
 def parse_input(path : str) -> List[Tuple[Range, Range]]:
     result = []
     with io.open(path, 'r') as file:
         for line in file.readlines():
             values = re.split(r',|-', line)
-            result.append((Range(values[0], values[1]), Range(values[2], values[3])))
+            result.append((Range(int(values[0].strip()), int(values[1].strip())), Range(int(values[2].strip()), int(values[3].strip()))))
 
     return result
 
@@ -28,6 +31,7 @@ if __name__ == "__main__":
     count = 0 
     for r in ranges:
         if(r[0].contains(r[1]) or r[1].contains(r[0])):
+            print(r[0], r[1])
             count += 1
     
     print(count)
