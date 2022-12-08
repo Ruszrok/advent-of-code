9be7fce4c9d79132ad39d09bb9c88701d0a9c61a
Ruszrok <judanov.v@gmail.com>
2022-12-09 00:12:29 +0700

    Initial implementation

--- 2022/day 8/main.go
+++ 2022/day 8/main.go
@@ -37,6 +37,66 @@ func ParseInput(pathToFile string) *[][]int {
 	return &result
 }
 
+func InitializeVisibilityMap(forest *[][]int) (*[][]int, int, int) {
+	numRows := len(*forest)
+	numColumns := len((*forest)[0])
+	visibilityMap := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		visibilityMap[i] = make([]int, numColumns)
+	}
+	return &visibilityMap, numRows, numColumns
+}
+
+func IsVisible(forest *[][]int, posX, posY, rowC, colC int) int {
+	if posX == 0 || posX == rowC-1 || posY == 0 || posY == colC-1 {
+		return 1
+	}
+
+	f := *forest
+	height := f[posX][posY]
+	visible := true
+	for i := posY - 1; i >= 0; i-- {
+		if f[posX][i] >= height {
+			visible = false
+			break
+		}
+	}
+	if visible {
+		return 1
+	}
+	visible = true
+	for i := posY + 1; i < colC; i++ {
+		if f[posX][i] >= height {
+			visible = false
+			break
+		}
+	}
+
+	visible = true
+	for i := posX - 1; i >= 0; i-- {
+		if f[i][posY] >= height {
+			visible = false
+			break
+		}
+	}
+	if visible {
+		return 1
+	}
+
+	visible = true
+	for i := posX + 1; i < rowC; i++ {
+		if f[i][posY] >= height {
+			visible = false
+			break
+		}
+	}
+	if visible {
+		return 1
+	}
+
+	return 0
+}
+
 func main() {
 	isTestFile := false
 	flag.BoolVar(&isTestFile, "t", false, "display in uppercase")
@@ -45,12 +105,18 @@ func main() {
 	if !isTestFile {
 		inputFileName = "input.txt"
 	}
-	parsed := ParseInput(inputFileName)
-	for _, v := range *parsed {
-		for _, h := range v {
-			fmt.Printf("%d", h)
-		}
-		fmt.Printf("\n")
-	}
+	parsedInput := ParseInput(inputFileName)
 
+	// Initialize visibility map
+	visibilityMap, rowsC, columnsC := InitializeVisibilityMap(parsedInput)
+
+	count := 0
+	for i := 0; i < rowsC; i++ {
+		for j := 0; j < columnsC; j++ {
+			(*visibilityMap)[i][j] = IsVisible(parsedInput, i, j, rowsC, columnsC)
+			count += (*visibilityMap)[i][j]
+		}
+	}
+	fmt.Println(*visibilityMap)
+	fmt.Println(count)
 }
