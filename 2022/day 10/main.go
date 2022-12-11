ce6b666d51a330129fe83c6e8008fd7ba9803b03
Ruszrok <judanov.v@gmail.com>
2022-12-12 01:04:03 +0700

    First part impl;emented

---
+++ 2022/day 10/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Operations string
+
+const (
+	addx Operations = "addx"
+	noop Operations = "noop"
+)
+
+type Operation struct {
+	name Operations
+	args int
+}
+
+func InitOperation(name Operations, args int) *Operation {
+	o := new(Operation)
+	o.name = name
+	o.args = args
+	return o
+}
+
+func ParseInput(pathToFile string) []*Operation {
+	f, err := os.Open(pathToFile)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to open file: %s", pathToFile))
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	var result = []*Operation{}
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), "\n")
+		if line == string(noop) {
+			result = append(result, InitOperation(noop, 0))
+		} else {
+			split := strings.Split(line, " ")
+			c, err := strconv.Atoi(split[1])
+			if err != nil {
+				panic(fmt.Sprintf("Error while parsing string %s", line))
+			}
+			result = append(result, InitOperation(addx, c))
+		}
+	}
+
+	return result
+}
+
+func dequeue(o []*Operation) (*Operation, []*Operation) {
+	if len(o) == 1 {
+		return o[0], []*Operation{}
+	}
+
+	return o[0], o[1:]
+}
+
+func main() {
+	isTestFile := false
+	flag.BoolVar(&isTestFile, "t", false, "display in uppercase")
+	flag.Parse()
+	inputFileName := "test.txt"
+	if !isTestFile {
+		inputFileName = "input.txt"
+	}
+
+	operations := ParseInput(inputFileName)
+
+	op_costs := make(map[Operations]int)
+	op_costs[noop] = 1
+	op_costs[addx] = 2
+
+	sum := 0
+	next_control := 20
+	x := 1
+	cycleCount := 1
+	currentOpCost := 1
+	current := InitOperation(noop, 0)
+	for cycleCount <= 220 && len(operations) > 0 {
+		currentOpCost -= 1
+		if currentOpCost == 0 {
+			switch current.name {
+			case addx:
+				x += current.args
+			}
+
+			current, operations = dequeue(operations)
+			switch current.name {
+			case noop:
+				currentOpCost = op_costs[noop]
+			case addx:
+				currentOpCost = op_costs[addx]
+			}
+		}
+		if cycleCount == next_control {
+			sum += cycleCount * x
+			next_control += 40
+		}
+		cycleCount += 1
+	}
+
+	fmt.Println(sum)
+}
