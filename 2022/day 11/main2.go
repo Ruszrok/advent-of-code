b11a7589dc367c4bb8fd7bc78b4a4bd841d35a0c
Ruszrok <judanov.v@gmail.com>
2022-12-14 11:01:13 +0700

    Separate file for 2nd part

---
+++ 2022/day 11/main2.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math/big"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Operation struct {
+	opearationLiteral      string
+	operationArgumentLeft  string
+	operationArgumentRight string
+}
+
+func (op *Operation) execute(oldValue *big.Int) *big.Int {
+	left, right := oldValue, oldValue
+	if op.operationArgumentLeft != "old" {
+		left_i, _ := strconv.Atoi(op.operationArgumentLeft)
+		left = big.NewInt(int64(left_i))
+	}
+	if op.operationArgumentRight != "old" {
+		right_i, _ := strconv.Atoi(op.operationArgumentRight)
+		right = big.NewInt(int64(right_i))
+	}
+	switch op.opearationLiteral {
+	case "*":
+		return left.Mul(left, right)
+	case "+":
+		return left.Add(left, right)
+	}
+	panic("Error in operations literal")
+}
+
+type Test struct {
+	divisibleTestValue *big.Int
+	SuccessTarget      int
+	FailureTatget      int
+	ExecutionCount     int
+}
+
+func (t *Test) try(worryLevel *big.Int) int {
+	t.ExecutionCount++
+	var res = big.NewInt(0)
+	res = res.Rem(worryLevel, t.divisibleTestValue)
+	if res.BitLen() == 0 {
+		return t.SuccessTarget
+	}
+
+	return t.FailureTatget
+}
+
+type Monkey struct {
+	number    string
+	items     []*big.Int
+	operation *Operation
+	test      *Test
+}
+
+func PreconditionTest(m *Monkey) {
+	if m == nil || m.items == nil || m.operation == nil || m.test == nil {
+		panic("Partial nil is nill")
+	}
+}
+
+func ParseInput(pathToFile string) []*Monkey {
+	f, err := os.Open(pathToFile)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to open file: %s", pathToFile))
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	result := []*Monkey{}
+	var m *Monkey = nil
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), "\n")
+		if strings.HasPrefix(line, "Monkey ") {
+			if m != nil {
+				PreconditionTest(m)
+				result = append(result, m)
+			}
+
+			m = new(Monkey)
+			m.items = *new([]*big.Int)
+
+			m.number = line[len("Monkey ") : len("Monkey ")+1]
+		}
+
+		if strings.HasPrefix(line, "  Starting items: ") {
+			l := line[len("  Starting items: "):]
+			for _, w := range strings.Split(l, ", ") {
+				v, err := strconv.Atoi(w)
+				if err != nil {
+					panic(fmt.Sprintf("Error in items parsing %s", w))
+				}
+
+				m.items = append(m.items, big.NewInt(int64(v)))
+			}
+		}
+
+		if strings.HasPrefix(line, "  Operation: new = ") {
+			l := line[len("  Operation: new = "):]
+			parts := strings.Split(l, " ")
+			if len(parts) > 3 {
+				panic(fmt.Sprintf("Parsing error in operation %s", l))
+			}
+
+			op := new(Operation)
+			op.operationArgumentLeft = parts[0]
+			op.opearationLiteral = parts[1]
+			op.operationArgumentRight = parts[2]
+			m.operation = op
+		}
+
+		if strings.HasPrefix(line, "  Test: divisible by ") {
+			l := line[len("  Test: divisible by "):]
+			v, err := strconv.Atoi(l)
+			if err != nil {
+				panic(fmt.Sprintf("Error in parsing %s", l))
+			}
+
+			t := new(Test)
+			t.divisibleTestValue = big.NewInt(int64(v))
+			t.ExecutionCount = 0
+			m.test = t
+		}
+
+		if strings.HasPrefix(line, "    If true: throw to monkey ") {
+			l := line[len("    If true: throw to monkey "):]
+			v, err := strconv.Atoi(l)
+			if err != nil {
+				panic(fmt.Sprintf("Error in parsing %s", l))
+			}
+
+			m.test.SuccessTarget = v
+		}
+
+		if strings.HasPrefix(line, "    If false: throw to monkey ") {
+			l := line[len("    If false: throw to monkey "):]
+			v, err := strconv.Atoi(l)
+			if err != nil {
+				panic(fmt.Sprintf("Error in parsing %s", l))
+			}
+
+			m.test.FailureTatget = v
+		}
+	}
+
+	if m != nil {
+		PreconditionTest(m)
+		result = append(result, m)
+	}
+
+	return result
+}
+
+func dequeue(o []*big.Int) (*big.Int, []*big.Int) {
+	if len(o) == 1 {
+		return o[0], []*big.Int{}
+	}
+
+	return o[0], o[1:]
+}
+
+func main() {
+	isTestFile := false
+	flag.BoolVar(&isTestFile, "t", false, "display in uppercase")
+	flag.Parse()
+	inputFileName := "test.txt"
+	if !isTestFile {
+		inputFileName = "input.txt"
+	}
+
+	monkeys := ParseInput(inputFileName)
+
+	solveSecondPart(monkeys)
+
+	fmt.Printf("Current operation %d\n", 10000)
+	PrintItemsCount(monkeys)
+
+	sort.Slice(monkeys, func(i, j int) bool {
+		return monkeys[i].test.ExecutionCount > monkeys[j].test.ExecutionCount
+	})
+
+	power := int64(monkeys[0].test.ExecutionCount) * int64(monkeys[1].test.ExecutionCount)
+	fmt.Println(power)
+}
+
+func solveSecondPart(monkeys []*Monkey) {
+	maxCount := 10000
+	for i := 0; i < maxCount; i++ {
+		if i%100 == 0 {
+			fmt.Printf("Current operation %d\n", i)
+			PrintItemsCount(monkeys)
+		}
+		for j := 0; j < len(monkeys); j++ {
+			m := monkeys[j]
+			item := big.NewInt(-1)
+			for len(m.items) > 0 {
+				item, m.items = dequeue(m.items)
+				nextlevel := m.operation.execute(item)
+				nextMonkey := m.test.try(nextlevel)
+				monkeys[nextMonkey].items = append(monkeys[nextMonkey].items, nextlevel)
+			}
+		}
+	}
+}
+
+func PrintItemsCount(ms []*Monkey) {
+	for i := 0; i < len(ms); i++ {
+		fmt.Printf("Monkey %d tested %d items\n", i, ms[i].test.ExecutionCount)
+	}
+}
